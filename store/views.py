319acfad454b1efa6cf997169dc4d7503ffd6dbd
erichou1 <eric@Erics-MBP.hsd1.ca.comcast.net>
2023-06-08 00:29:49 -0700

    Filled in UI elements

--- store/views.py
+++ store/views.py
@@ -12,6 +12,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 import json
+import smtplib 
 # Create your views here.
 from .models import *
 
@@ -33,10 +34,9 @@ def myajaxtestview(request):
     
     content = eval(request.POST['text'])
     print(content)
-    import smtplib 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:  
-        email_address = 'hackxhackathon@gmail.com'
-        email_password = 'cvzolwemcagnqgol'
+        email_address = 'losgatoshacks@gmail.com'
+        email_password = 'cnqlkhzkqsmiywnu'
         connection.login(email_address, email_password )
         connection.sendmail(from_addr=email_address, to_addrs=content['student_email'], 
         msg="subject:Los Gatos Hacks Registration Confirmation \n\n    Thanks for registering for the 2023 Los Gatos Hacks Hackathon on 9/9-9/10. We can't wait to see what amazing creations you can bring to life! \n    If you have any questions, please email us at losgatoshacks@gmail.com. \n\nKind regards, \nLos Gatos Hacks Team")
