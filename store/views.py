6030955a230e06cbb897f34f79cd3edd7acaf9ca
Edwin <edwin.s.hou@gmail.com>
2023-06-08 18:46:34 -0700

    Integrated Google Spreadsheet API for Registration Data

--- store/views.py
+++ store/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 import json
 import smtplib 
+import gspread
 # Create your views here.
 from .models import *
 
@@ -31,14 +32,21 @@ def form(request):
     return render(request, 'store/form.html')
 @csrf_exempt    
 def myajaxtestview(request):
+    sa = gspread.service_account(filename="services_account.json")
+    sh = sa.open("Los Gatos Hacks Emails")
+    wks = sh.worksheet("Registrations")
+    # sh.add_worksheet(title="Registrations", rows=1, cols=9)
     
-    content = eval(request.POST['text'])
-    print(content)
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:  
-        email_address = 'losgatoshacks@gmail.com'
-        email_password = 'cnqlkhzkqsmiywnu'
-        connection.login(email_address, email_password )
-        connection.sendmail(from_addr=email_address, to_addrs=content['student_email'], 
-        msg="subject:Los Gatos Hacks Registration Confirmation \n\n    Thanks for registering for the 2023 Los Gatos Hacks Hackathon on 9/9-9/10. We can't wait to see what amazing creations you can bring to life! \n    If you have any questions, please email us at losgatoshacks@gmail.com. \n\nKind regards, \nLos Gatos Hacks Team")
-
-    return HttpResponse(request.POST['text'])
+    content = eval(request.POST.get("data"))
+    # print(content.keys())
+    wks.append_row(list(content.values()))    
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:  
+            email_address = 'losgatoshacks@gmail.com'
+            email_password = 'cnqlkhzkqsmiywnu'
+            connection.login(email_address, email_password )
+            connection.sendmail(from_addr=email_address, to_addrs=content['student_email'], 
+            msg="subject:Los Gatos Hacks Registration Confirmation \n\n    Thanks for registering for the 2023 Los Gatos Hacks Hackathon on 9/9-9/10. We can't wait to see what amazing creations you can bring to life! \n    If you have any questions, please email us at losgatoshacks@gmail.com. \n\nKind regards, \nLos Gatos Hacks Team")
+    except:
+        pass
+    return HttpResponse(request.POST.get("data"))
