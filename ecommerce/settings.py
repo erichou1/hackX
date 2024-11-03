96c6284ddedd5c8695d7d6d4997e60c981d4e2a8
EricHou1 <eric.x.hou@gmail.com>
2024-11-02 18:05:37 -0700

    a

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^4)*fp%$7h)b+jz18up9t81j@u@kb=+5-++em^7u#zs#hu4&bv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
